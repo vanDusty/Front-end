ff1f1c227f10dd026a4532a8ed40dddc31299018
Van <1251787298@qq.com>
2020-04-10 17:17:34 +0800

    Mybatis 配置多数据源最简解决方案

---
+++ mybatis-case/multipledata-simple/src/main/java/cn/van/mybatis/multipledata/simple/mapper/master/User1Mapper.java
@@ -0,0 +1,8 @@
+package cn.van.mybatis.multipledata.simple.mapper.master;
+
+import cn.van.mybatis.multipledata.simple.entity.User;
+
+public interface User1Mapper {
+    void insert(User user);
+
+}
---
+++ mybatis-case/multipledata-simple/src/main/java/cn/van/mybatis/multipledata/simple/mapper/slave/User2Mapper.java
@@ -0,0 +1,9 @@
+package cn.van.mybatis.multipledata.simple.mapper.slave;
+
+import cn.van.mybatis.multipledata.simple.entity.User;
+
+
+public interface User2Mapper {
+    void insert(User user);
+
+}
