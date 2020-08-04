44da1ec749ad6fd104be2192075e622c3056544c
Van <1251787298@qq.com>
2020-08-04 13:59:50 +0800

    init

--- mybatis-case/multipledata-simple/src/main/java/cn/van/mybatis/multipledata/simple/mapper/master/User1Mapper.java
+++
@@ -1,8 +0,0 @@
-package cn.van.mybatis.multipledata.simple.mapper.master;
-
-import cn.van.mybatis.multipledata.simple.entity.UserInfo;
-
-public interface User1Mapper {
-    void insert(UserInfo userInfo);
-
-}
--- mybatis-case/multipledata-simple/src/main/java/cn/van/mybatis/multipledata/simple/mapper/slave/User2Mapper.java
+++
@@ -1,9 +0,0 @@
-package cn.van.mybatis.multipledata.simple.mapper.slave;
-
-import cn.van.mybatis.multipledata.simple.entity.UserInfo;
-
-
-public interface User2Mapper {
-    void insert(UserInfo userInfo);
-
-}
