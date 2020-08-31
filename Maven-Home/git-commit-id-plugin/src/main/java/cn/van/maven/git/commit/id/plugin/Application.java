612f1211bdc7fdf1e076c851d66e9b8f0a081c37
Van <1251787298@qq.com>
2020-08-31 10:27:35 +0800

    maven init

---
+++ Maven-Home/git-commit-id-plugin/src/main/java/cn/van/maven/git/commit/id/plugin/Application.java
@@ -0,0 +1,11 @@
+package cn.van.maven.git.commit.id.plugin;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(DruidApplication.class, args);
+    }
+}
